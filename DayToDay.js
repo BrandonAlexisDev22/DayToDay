694817fc8b93faa7b5b8b8220a60e6ff97dc8883
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-10-10 21:02:00 -0500

    October 10

--- DayToDay.js
+++ DayToDay.js
@@ -1,2 +1,3 @@
 console.log('October 8th Very happy Starting React and doing my habits');
-console.log('October 9th I had some errors doing my habits but I feel so happy')
+console.log('October 9th I had some errors doing my habits but I feel so happy');
+console.log('October 10 hmmm regular day but I starting feel god because at least :D');
