17dd4cee939c05b68b7539f0d373236e94e0eca7
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-10-16 23:05:52 -0500

    October 16th

--- DayToDay.js
+++ DayToDay.js
@@ -5,4 +5,5 @@ console.log('October 11 Very regular day but I spend time with my grandmother');
 console.log('October 12 Full Work but I feel so sad for my status.');
 console.log('October 13th I feel good with the return of my process good luck');
 console.log('October 14th very so tired but lets go!');
-console.log('October 15th Im very sad for my grandmother his feeling its very sad. but i trust in my god')
+console.log('October 15th Im very sad for my grandmother his feeling its very sad. but i trust in my god');
+console.log('October 16th Thinking about my life in my future but also in the respect.');
