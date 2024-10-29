edf3a73e02eedebcacde3af7c214aab947f5b547
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-10-28 22:51:33 -0500

    October 28th

--- DayToDay.js
+++ DayToDay.js
@@ -18,3 +18,4 @@ console.log('October 24th mt is doneeeeeeeeeeeeeeeeeeeeeeee. ');
 console.log('October 25th maybe tired but i need following this path');
 console.log('October 26th working in the game, lets go!');
 console.log('October 27th tired but lets go with ingeniatec');
+console.log('October 28th finishing the lab... the game')
