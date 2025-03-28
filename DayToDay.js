1586da8844dc187d36353f493f84578a1901c047
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-03-27 21:04:04 -0500

    March 27th

--- DayToDay.js
+++ DayToDay.js
@@ -1,67 +1,81 @@
-console.log('October 8th Very happy Starting React and doing my habits');
-console.log('October 9th I had some errors doing my habits but I feel so happy');
-console.log('October 10 hmmm regular day but I starting feel god because at least :D');
-console.log('October 11 Very regular day but I spend time with my grandmother');
-console.log('October 12 Full Work but I feel so sad for my status.');
-console.log('October 13th I feel good with the return of my process good luck');
-console.log('October 14th very so tired but lets go!');
-console.log('October 15th Im very sad for my grandmother his feeling its very sad. but i trust in my god');
-console.log('October 16th Thinking about my life in my future but also in the respect.');
-console.log('October 17th A day very good but very less work but i feel blessed');
-console.log('October 18th Idk :( good but...');
-console.log('October 19th I dont have energy, i feel so tired but i want to improve but the lazy is very strong');
-console.log('October 20th day of play football with my friends :D');
-console.log('October 21th wtf idk it passing for my head!');
-console.log('October 22th i dont know');
-console.log('October 23th fun day with my friends but i did not work :(');
-console.log('October 24th mt is doneeeeeeeeeeeeeeeeeeeeeeee. ');
-console.log('October 25th maybe tired but i need following this path');
-console.log('October 26th working in the game, lets go!');
-console.log('October 27th tired but lets go with ingeniatec');
-console.log('October 28th finishing the lab... the game');
-console.log('October 29th a normal day but i feel tired! mttt')
-console.log('October 30th great day in ingeniatec but I lost a opportunity with a girl very beautifull!! greattttt');
-console.log('October 31th good day but its time to start the path');
-console.log('November 1th fxck day, what is this shit?');
-console.log('November 2th to 4th lets go with the process.....');
-console.log('November 5th so sooooooooooo...');
-console.log('November 6th I need to think things through...');
-console.log('November 16th returning after 10 days');
-console.log('November 18th time to change????????');
-console.log('November 19th love not boy love not hahahaha');
-console.log('November 20th hahaha trying new proyect');
-console.log('November 21th I feel so bad but I have to disappear...');
-console.log('November 25th Lets goooooo')
-console.log('December 1th Monk Mode day one good..')
-console.log('December 2th Monk mode rest...');
-console.log('December 3th just starting...');
-console.log('December 4th I have a good day');
-console.log('December 5th Great day yeah');
-console.log('December 6th this situation its hard but... good day.');
-console.log('December 7th Im proud for my self, lets goo')
-console.log('December 8th good day haaha');
-console.log('December 9th other good day brooo');
-console.log('December 10th good dayyyy my god')
-console.log('December 11th god day broo');
-console.log('December 12th stfuff yeah');
-console.log('December 13th finishing the project... :D');
-console.log('December 14th maybe idk :(');
-console.log('December 15th nothing bad...');
-console.log('December 16th idk lets goooo');
-console.log('December 17th good day :)))');
-console.log('December 18th idk idk');
-console.log('December 19th good day but i dont feel good');
-console.log('December 20th rest in peace bro');
-console.log('December 21th idk idk');
-console.log('January 24th return jaajjaj');
-console.log('February 2th waiting...');
+console.log("October 8th Very happy Starting React and doing my habits");
+console.log(
+  "October 9th I had some errors doing my habits but I feel so happy"
+);
+console.log(
+  "October 10 hmmm regular day but I starting feel god because at least :D"
+);
+console.log("October 11 Very regular day but I spend time with my grandmother");
+console.log("October 12 Full Work but I feel so sad for my status.");
+console.log("October 13th I feel good with the return of my process good luck");
+console.log("October 14th very so tired but lets go!");
+console.log(
+  "October 15th Im very sad for my grandmother his feeling its very sad. but i trust in my god"
+);
+console.log(
+  "October 16th Thinking about my life in my future but also in the respect."
+);
+console.log(
+  "October 17th A day very good but very less work but i feel blessed"
+);
+console.log("October 18th Idk :( good but...");
+console.log(
+  "October 19th I dont have energy, i feel so tired but i want to improve but the lazy is very strong"
+);
+console.log("October 20th day of play football with my friends :D");
+console.log("October 21th wtf idk it passing for my head!");
+console.log("October 22th i dont know");
+console.log("October 23th fun day with my friends but i did not work :(");
+console.log("October 24th mt is doneeeeeeeeeeeeeeeeeeeeeeee. ");
+console.log("October 25th maybe tired but i need following this path");
+console.log("October 26th working in the game, lets go!");
+console.log("October 27th tired but lets go with ingeniatec");
+console.log("October 28th finishing the lab... the game");
+console.log("October 29th a normal day but i feel tired! mttt");
+console.log(
+  "October 30th great day in ingeniatec but I lost a opportunity with a girl very beautifull!! greattttt"
+);
+console.log("October 31th good day but its time to start the path");
+console.log("November 1th fxck day, what is this shit?");
+console.log("November 2th to 4th lets go with the process.....");
+console.log("November 5th so sooooooooooo...");
+console.log("November 6th I need to think things through...");
+console.log("November 16th returning after 10 days");
+console.log("November 18th time to change????????");
+console.log("November 19th love not boy love not hahahaha");
+console.log("November 20th hahaha trying new proyect");
+console.log("November 21th I feel so bad but I have to disappear...");
+console.log("November 25th Lets goooooo");
+console.log("December 1th Monk Mode day one good..");
+console.log("December 2th Monk mode rest...");
+console.log("December 3th just starting...");
+console.log("December 4th I have a good day");
+console.log("December 5th Great day yeah");
+console.log("December 6th this situation its hard but... good day.");
+console.log("December 7th Im proud for my self, lets goo");
+console.log("December 8th good day haaha");
+console.log("December 9th other good day brooo");
+console.log("December 10th good dayyyy my god");
+console.log("December 11th god day broo");
+console.log("December 12th stfuff yeah");
+console.log("December 13th finishing the project... :D");
+console.log("December 14th maybe idk :(");
+console.log("December 15th nothing bad...");
+console.log("December 16th idk lets goooo");
+console.log("December 17th good day :)))");
+console.log("December 18th idk idk");
+console.log("December 19th good day but i dont feel good");
+console.log("December 20th rest in peace bro");
+console.log("December 21th idk idk");
+console.log("January 24th return jaajjaj");
+console.log("February 2th waiting...");
 console.log(`February 3th good day...`);
-console.log("February 4th rejected....")
-console.log("February 22th return???")
+console.log("February 4th rejected....");
+console.log("February 22th return???");
 console.lo("February 24th learning python agaih");
 console.log("February 25th good?");
 console.log("February 26th I feel so bad with my self image");
-console.log("February 27th goo day :))")
+console.log("February 27th goo day :))");
 console.log("February 28th god dayyy");
 console.log("March 1th thinking?????");
 console.log("March 5th good day");
@@ -70,7 +84,8 @@ console.log("March 15th idk again");
 console.log("March 17th Good day");
 console.log("March 20th i need thing my path");
 console.log("March 21th Great day");
-console.log("March 22th Happy birth Dayyyyy")
-console.log("March 24th return returnnnnnn")
-console.log("March 25th exam approved")
-console.log("March 26th confuseD???")
+console.log("March 22th Happy birth Dayyyyy");
+console.log("March 24th return returnnnnnn");
+console.log("March 25th exam approved");
+console.log("March 26th confuseD???");
+console.log("March 27th i feel in luv");
