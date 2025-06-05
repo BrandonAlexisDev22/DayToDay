6e0a6801c86b46682bcf35ac3266d79709a25e71
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-06-04 21:19:57 -0500

    Add entries for June 3rd and June 4th in DayToDay.js

--- DayToDay.js
+++ DayToDay.js
@@ -155,4 +155,5 @@ console.log("May 29th god please care my grandmother please :(");
 console.log("May 30th gd dayy");
 console.log("June 1th nt bad")
 console.log("June 2th lets go prepare...");
-console.log("June 3th gd day ggggg")
+console.log("June 3th gd day ggggg");
+console.log("June 4th day with my grandmotherrr")
