12aa5ea948ba5a0e785634aa75e3b5f34ee5e7af
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-07-02 20:33:51 -0500

    Add entries for June 28th to July 2nd in DayToDay.js

--- DayToDay.js
+++ DayToDay.js
@@ -184,3 +184,4 @@ console.log("June 28th ....")
 console.log('June 29th gd day');
 console.log('June 30th i fee so tried jajajajaja')
 console.log('July 1th start of objectives???')
+console.log("July 2th gd day ")
